bc0371c8982c9f17d15da2241de24b12140b15c4
esteban zambrano <ezam0000@gmail.com>
2019-03-26 17:34:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,32 +4,34 @@ dwarves.forEach(function(name, idx) {
 newStr += (idx+1 + ". " + name + " ");
 });
 return newStr;
+
 }
+
+
 function summonCaptainPlanet(planeteerCalls){
-   var newPlanet = [];
+var newPlanet = [];
 for(var i = 0; i < planeteerCalls.length; i++) {
-newPlanet.push(planeteerCalls[i].toUpperCase() + '!');
+  newPlanet.push(planeteerCalls[i].toUpperCase() + '!');
 }
 return newPlanet; 
 }
+
 
 function longPlaneteerCalls(words) {
 for(var i =0; i < words.length; i++) {
   if(words[i].length > 4) {
     return true; 
-    
-  } else{
-    return false; 
+  } 
 }
+return false; 
 }
-}
+
 function findTheCheese (foods) {
 for(var i =0; i < foods.length; i++) {
-if(foods[i] === 'cheddar' || foods[i] === 'gouda' || foods[i] === 'camembert') {
-return foods[i];
-} 
+  if(foods[i] === 'cheddar' || foods[i] === 'gouda' || foods[i] === 'camembert') {
+    return foods[i];
+  } 
 }
-if(foods !== 'cheddar' || foods !== 'gouda' || foods !== 'camembert') {
-return 'no cheese!';
+return "no cheese!"
 } 
-}
+
